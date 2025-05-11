20e44242250431346e81250d93d779837b808080
alaa2213 <124505830+alaa2213@users.noreply.github.com>
2025-05-11 13:02:51 +0300

    Update App.jsx with Evaluations.jsx path

--- Elevate/myapp/src/App.jsx
+++ Elevate/myapp/src/App.jsx
@@ -10,6 +10,7 @@ import StudentProfile from './students';
 import Internships from './internships';
 import MainLayout from './MainLayout';
 import StudentProfile from './studentProfile';
+import Evaluations from './Evaluations';
 
 function App() {
   return (
@@ -21,7 +22,7 @@ function App() {
         <Route path='/pro' element={<StudentDashboard />} />
        <Route element={<MainLayout />}>
     <Route path="/reports" element={<Reports />} />
-      
+       <Route path="/Evaluations" element={<Evaluations />} />
       <Route path="/scaddashboard" element={<ScadDashboard />} />
       <Route path="/internships" element={<Internships />} />
       <Route path="/students" element={<StudentList />} />
